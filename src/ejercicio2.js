f5f936ba906f3daf8e48ba49055bcf116065a36e
TitelChasa <cristelchavarria@hotmail.com>
2023-06-20 15:07:46 -0600

    agregar

--- src/ejercicio2.js
+++ src/ejercicio2.js
@@ -1,7 +1,17 @@
+import { contador } from "./ejercicio1";
+
 class Contador {
-  // Inserte el código aquí
+  valor=0
+
+siguente(){
+  this.valor +=1;
+}
 }
 
 // Inserte el código aquí
+const instanciaA = new contador();
+const instanciaB = new contador();
 
-export { instancia }
+instanciaA.siguente();
+console.log(instanciaA, instanciaB);
+
