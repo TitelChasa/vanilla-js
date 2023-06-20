8b7963204ba7ee2e970128e4f8e6d05e312433c6
TitelChasa <cristelchavarria@hotmail.com>
2023-06-20 15:07:07 -0600

    agregar

--- src/ejercicio1.js
+++ src/ejercicio1.js
@@ -1,8 +1,13 @@
 let contador = {
   valor: 0,
+  
   siguiente: function () {
-    // Inserte el código aquí
+  this.valor= this.valor +1
+  return this.valor
+
   },
 };
 
+
+console.log(contador.siguiente())
 export { contador }
