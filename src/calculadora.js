b3b36754c463b5bd686c9fe37778ba61186f467d
TitelChasa <cristelchavarria@hotmail.com>
2023-06-22 17:19:46 -0600

    guardado

---
+++ src/calculadora.js
@@ -0,0 +1,27 @@
+
+
+
+
+function suma(num,num2){
+
+
+    return num+num2;
+}
+ function resta(num,num2){
+    return num-num2
+ }
+
+  function multiplicar(num,num2){
+    return num*num2;
+  }
+
+function dividir(num,num2){
+
+    return num/num2;
+}
+
+
+
+let calculadora = {suma, resta, multiplicar, dividir };
+
+export{calculadora};
--- src/index.js
+++ src/index.js
@@ -1,2 +1,35 @@
 // Inserte el código aquí
 
+import { calculadora } from "./calculadora.js"
+
+
+function calcular (operacion, numero1, numero2){
+
+    if (operacion=="suma") {
+        
+        console.log(calculadora.suma(numero1,numero2))
+    }else{
+        if (operacion=="resta") {
+            console.log(calculadora.resta(numero1,numero2))
+        }
+    }
+if (operacion=="multiplicar") {
+    console.log(calculadora.multiplicar(numero1,numero2))
+}else{
+    if (operacion=="dividir") {
+        console.log(calculadora.dividir(numero1,numero2))
+    }
+}
+
+}
+
+
+calcular("dividir",4,7)
+
+
+
+
+
+
+
+
